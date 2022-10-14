56edac29b27a4be0ecee3af18888b78073d3a046
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-10-14 02:32:31 -0700

    Added swaping

---
+++ swaping.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelAndConsonant
+{
+    public class swaping
+    {
+        public static void SwapingTheNumbers()
+        {
+            int a = 2, b = 5;
+            Console.WriteLine("Before swap a=" + a + " b = " + b);
+
+            a = a * b;
+            b = a / b;
+            a = a / b;
+
+            Console.WriteLine("After swap a=" + a + " b = " + b);
+
+        }
+
+    }
+}
