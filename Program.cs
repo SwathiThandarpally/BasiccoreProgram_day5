761afb15a4fe7a2ad28461dc9ac979479c5cfed6
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-10-14 02:34:28 -0700

    Added program

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace VowelAndConsonant
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            {
+                //CheckAplphabet.Display();
+
+                //QuotientAndRemainder.CheckQuotientRemainder();
+
+                //swaping.SwapingTheNumbers();
+
+                //FindLargestNumber.CheckLargestNumber();
+
+                EVENorODD.CheckEvenOrOdd();
+            }
+        }
+    }
+}
