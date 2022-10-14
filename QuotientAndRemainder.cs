47ef96bcca9d2e1ac3e4e7f0875c34df3ab7cd54
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-10-14 02:30:35 -0700

    Added quotient and remainder

---
+++ QuotientAndRemainder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelAndConsonant
+{
+    public static class QuotientAndRemainder
+    {
+
+        public static void CheckQuotientRemainder()
+        {
+
+            int dividend = 50, divisor = 5;
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend : {0} Divisor : {1}", dividend, divisor);
+
+            Console.WriteLine("Dividend : "+ dividend+" Divisor : "+divisor);
+
+            Console.WriteLine("Quotient : " + quotient);
+
+            Console.WriteLine("Remainder : " + remainder);
+
+        }
+    }
+}
+
