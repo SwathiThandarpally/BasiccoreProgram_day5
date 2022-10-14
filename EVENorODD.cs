f42fad61849163c47a8a7be19dba08ed4622aa7c
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-10-14 02:22:46 -0700

    Added even or odd

---
+++ EVENorODD.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelAndConsonant
+{
+    public class EVENorODD
+    {
+        public static void CheckEvenOrOdd()
+        {
+            int a;
+            Console.WriteLine("Enter a number :");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.ReadLine();
+                Console.WriteLine("Enter the number is Even");
+             
+            }
+            else
+            {
+                Console.ReadLine();
+                Console.WriteLine("Enter the number is odd");
+               
+            }
+
+        }
+
+    }
+}
